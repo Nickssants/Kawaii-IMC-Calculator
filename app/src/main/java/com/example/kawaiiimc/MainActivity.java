132c149601b0a19f2bb38b97d7002153f391b46d
Nickssants <nicls.sants@gmail.com>
2023-07-25 11:12:30 -0300

    Initial commit

---
+++ app/src/main/java/com/example/kawaiiimc/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.kawaiiimc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TextView textResultado;
+    private TextView editPeso;
+    private TextView editAltura;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textResultado = findViewById(R.id.textResultado);
+        editPeso = findViewById(R.id.editPeso);
+        editAltura = findViewById(R.id.editAltura);
+
+    }
+
+    public void calcularImc (View view){
+
+        double peso = Double.parseDouble(editPeso.getText().toString());
+        double altura = Double.parseDouble(editAltura.getText().toString());
+        double resultado = peso / (altura * altura);
+
+        //se
+        if ( resultado < 19 ){
+            textResultado.setText("Abaixo do peso");
+        }else if ( resultado <= 19 || resultado < 25 ){
+            textResultado.setText("Peso normal");
+        }else if ( resultado <= 25 || resultado < 30 ){
+            textResultado.setText("Sobre peso");
+        }else if ( resultado <= 30 || resultado < 40 ){
+            textResultado.setText("Obesidade tipo I");
+        }else if ( resultado >= 40 ) {
+            textResultado.setText(("Obesidade tipo II"));
+        }
+    }
+
+
+}
